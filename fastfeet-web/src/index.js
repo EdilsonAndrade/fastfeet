88ecc12b14afc80bac4600305de360db51badb73
Edilson Andrade <edilson.a.andrade@gmail.com>
2020-02-25 16:17:34 -0300

    demonstrando como usar o git para o eduardo

--- fastfeet-web/src/index.js
+++ fastfeet-web/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 require('react-datepicker/dist/react-datepicker-cssmodules.css');
-
 require('react-datepicker/dist/react-datepicker.css');
 
 ReactDOM.render(<App />, document.getElementById('root'));
