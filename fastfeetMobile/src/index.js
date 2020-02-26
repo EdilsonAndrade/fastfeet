661e8d879420bca5dab5937f1dc775d25205cd9f
Edilson Andrade <edilson.a.andrade@gmail.com>
2020-02-25 21:55:49 -0300

    mobile fastfeet

---
+++ fastfeetMobile/src/index.js
@@ -0,0 +1,16 @@
+import {NavigationContainer} from '@react-navigation/native';
+import React from 'react';
+import {SafeAreaView, Text, StatusBar} from 'react-native';
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <Text>FastFeet</Text>
+      </SafeAreaView>
+    </NavigationContainer>
+  );
+};
+
+export default App;
