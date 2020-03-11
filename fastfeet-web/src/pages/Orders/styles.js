e35bf95b502cde61dc0b8bc770a0cade1d5897fa
Edilson Andrade <edilson.a.andrade@gmail.com>
2020-03-10 22:58:56 -0300

    last fix view

--- fastfeet-web/src/pages/Orders/styles.js
+++ fastfeet-web/src/pages/Orders/styles.js
@@ -71,5 +71,8 @@ export const DeliveryAvatar = styled.div`
     width: 30px;
     height: 30px;
     border-radius: 15px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
   }
 `;
